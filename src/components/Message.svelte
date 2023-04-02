e3306aa8e32f77875da1e3095b5cca1f417d7be4
slatinsky <slatinsky.adam@gmail.com>
2023-04-02 05:08:38 +0200

    format markdown

--- src/components/Message.svelte
+++ src/components/Message.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+    // import markdown-it
+    import MarkdownIt from "markdown-it";
+
+    const md = new MarkdownIt();
+
 	import type { Message } from "$ts/types";
 	import { conversationsManager } from "$ts/ConversationsManager";
     const selectedConversation = conversationsManager.selectedConversation
@@ -49,16 +54,31 @@
             <button on:click={setMemory} class="bg-gray-600 hover:bg-gray-500 px-1 py-1 text-sm">Set memory</button>
         {/if}
         <button on:click={deleteMessage} class="bg-gray-600 hover:bg-gray-500 px-1 py-1 text-sm">Delete message</button>
-        <button on:click={toggleRaw} class="bg-gray-600 hover:bg-gray-500 px-1 py-1 text-sm">Show raw</button>
+        <button on:click={toggleRaw} class="bg-gray-600 hover:bg-gray-500 px-1 py-1 text-sm">
+            {#if showRaw}
+                Show formatted
+            {:else}
+                Show raw
+            {/if}
+        </button>
     </div>
 
     {#if showRaw}
         <pre class="px-4 pt-2 pb-8 text-gray-200">{message.content.replace(/</g, "&lt;").replace(/>/g, "&gt;")}</pre>
     {:else}
-        <div class="px-4 pt-2 pb-8 text-gray-200">
-            {message.content}
+        <div class="px-4 pt-2 pb-8 text-gray-200 message-content">
+            {@html md.render(message.content)}
         </div>
     {/if}
 </div>
 
+
+<style>
+    .message-content :global(pre) {
+        background-color: #1a202c;
+        border-radius: 0.25rem;
+        padding: 1rem;
+        margin: 1rem 0;
+    }
+</style>
 
