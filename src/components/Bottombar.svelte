4c5111835ce5bdce2cd270743641a058d49d588f
slatinsky <slatinsky.adam@gmail.com>
2023-08-09 18:54:11 +0200

    add quick model version switcher

--- src/components/Bottombar.svelte
+++ src/components/Bottombar.svelte
@@ -4,7 +4,7 @@
 	import { scrollDown } from "$ts/helpers";
 	import { ai } from "$ts/openai/ai";
 	import type { Writable } from "svelte/store";
-	import Message from "./Message.svelte";
+	import { apiModel } from "$ts/stores/settingsStores";
 
 
     let message = "";
@@ -126,7 +126,19 @@
     <textarea on:keydown={keydown} class="w-full bg-gray-500 px-2" name="" id="" rows="3" placeholder="Type your message here. To insert new line, press shift+enter." bind:value={message} />
     <button on:click={send} class="bg-gray-600 hover:bg-gray-500 px-1 py-1">Send <span class="text-xs text-gray-300">[enter]</span></button>
     <button on:click={sendWithoutHistory} class="bg-gray-600 hover:bg-gray-500 px-1 py-1">Send without history <span class="text-xs text-gray-300">[ctrl+enter]</span></button>
+    <div class="flex flex-col  items-stretch content-stretch">
+        <button on:click={() => $apiModel = "gpt-4" } class:active={$apiModel == "gpt-4"} class="bg-gray-600 hover:bg-gray-500 px-1 py-1 flex-1" title="gpt-4">4</button>
+        <button on:click={() => $apiModel = "gpt-3.5-turbo" } class:active={$apiModel == "gpt-3.5-turbo"} class="bg-gray-600 hover:bg-gray-500 px-1 py-1 flex-1" title="gpt-3.5-turbo">3.5</button>
+    </div>
+
     <!-- <button on:click={scrollDown} class="bg-gray-600 hover:bg-gray-500 px-1 py-1">Scroll down</button> -->
     <!-- <button on:click={simulateUser} class="bg-gray-600 hover:bg-gray-500 px-1 py-1">Simulate user</button> -->
     <!-- <button on:click={simulateAssistant} class="bg-gray-600 hover:bg-gray-500 px-1 py-1">Simulate ChatGPT</button> -->
 </div>
+
+<style>
+    button.active {
+        background-color: #1a202c;
+        outline: none;
+    }
+</style>
